174467c756d19af3e86c6563ec55a35a9eaf0d24
George <tirion77@gmail.com>
2019-02-16 10:31:58 -0800

    added draft of the contract

---
+++ contracts/VolunteersDilemma.sol
@@ -0,0 +1,58 @@
+pragma solidity ^0.5.4;
+
+contract VolunteersDilemma {
+    address owner;
+    uint bet = 1000 wei;
+    uint player_number = 3;
+    uint current_players = 0;
+    
+    constructor public {
+        owner = msg.sender;
+    } 
+    
+    struct Player {
+        bool reported;
+        bool voted;
+        address addr;
+        uint payout;
+        bool withdrawn;
+    }
+
+    
+    mapping (address => Player) players;
+    
+
+    modifier new_player {
+        //make sure the player is new
+        require (players[msg.sender].voted==false);
+        _;
+    }
+    
+    function act(bool _reported) public payable new_player {
+        players[msg.sender].voted = true;
+        players[msg.sender].reported = _reported;
+        players[msg.sender].addr = msg.sender;
+        
+        //do stuff
+        
+        //check if max player reached
+        current_players++;
+        if (current_players == player_number){
+            decide_winner();
+        }
+    }
+    
+    function decide_winner() internal {
+        //find out what happened
+        //...
+        outcome = {1,2,3}
+        
+        1:
+            everybody wins
+        2:
+            everybody loses
+        3:
+            iterate through each user's payout
+        
+    }
+}
