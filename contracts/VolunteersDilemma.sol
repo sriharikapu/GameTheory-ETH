1be32f4d32cfc6fa3f78c4c42be3f57238a997e9
Grant <grantpaulson6@gmail.com>
2019-02-16 18:50:25 -0700

    update mapping scheme from Grants comp

--- contracts/VolunteersDilemma.sol
+++ contracts/VolunteersDilemma.sol
@@ -6,33 +6,26 @@ import "./Withdrawal.sol";
 
 contract VolunteersDilemma is Withdrawal {
     address owner;
-    address[] player_addresses;
-    uint current_players;
+    address[3] player_addresses
+    uint current_player;
     uint bet;
-    uint player_number;
+    uint player_total;
     
     event Results(uint _ignorers, uint _reporters);
     
     constructor() public {
         owner = msg.sender;
         bet = 1000 wei;
-        player_number = 3;
-        
+        player_total = 3;
+        current_player = 0;
     } 
     
-    struct Player {
-        bool reported;
-        bool voted;
-        address addr;
-    }
-
-    
-    mapping (address => Player) players;
+    mapping (address => uint8) players_vote;
     
 
     modifier new_player {
         //make sure the player is new
-        require (players[msg.sender].voted==false);
+        require (players_vote[msg.sender]==0);
         _;
     }
     
@@ -42,16 +35,13 @@ contract VolunteersDilemma is Withdrawal {
     }
     
     
-    function act(bool _reported) public payable new_player enough_money{
-        player_addresses.push(msg.sender);
-        players[msg.sender].voted = true;
-        players[msg.sender].reported = _reported;
-        players[msg.sender].addr = msg.sender;
-        
+    function act(uint8 _reported) public payable new_player enough_money{
+        players_vote[msg.sender] = _reported; // reported should be 1 or 2
+        player_addresses[current_player] = msg.sender
         //check if end of voting period is here
         //currently based on max player reached
-        current_players++;
-        if (current_players == player_number){
+        current_player++;
+        if (current_player == player_total){
             decide_winner();
         }
     }
@@ -61,35 +51,50 @@ contract VolunteersDilemma is Withdrawal {
         uint reporters;
         uint ignorers;
         uint shared_pot;
-        for (uint i=0; i< player_number; i++){
-            if(players[player_addresses[i]].reported){
+
+        //figure out score
+        for (uint i=0; i < player_total; i++){
+            if(players_vote[player_addresses[i]].reported){
                 reporters++;
             }
             else{
                 ignorers++;
             }
         }
+
+        //distribute winnings based on score
         if (reporters == 0){
             //transfer money to the mastercontract/bank;
+            reset_game()
         }
         else if (ignorers == 0){
-            for (uint i=0; i< player_number; i++){
-                if (players[player_addresses[i]].reported){
-                    pendingWithdrawal[player_addresses[i]]=bet + (bet/player_number);
+            for (uint i=0; i< player_total; i++){
+                if (players_vote[player_addresses[i]].reported){
+                    pendingWithdrawal[player_addresses[i]]=bet + (bet/player_total);
                     //figure out withdrawing from the pot
                 }
             }
+            reset_game()
         }
         else {
-            for (uint i=0; i< player_number; i++){
-                if (players[player_addresses[i]].reported){
-                    pendingWithdrawal[player_addresses[i]]=(bet/player_number);
+            for (uint i=0; i< player_total; i++){
+                if (players_vote[player_addresses[i]].reported){
+                    pendingWithdrawal[player_addresses[i]]=(bet/player_total);
                 }
                 else{
-                    shared_pot = (bet/player_number)*(player_number*reporters-reporters);
+                    shared_pot = (bet/player_total)*(player_total*reporters-reporters);
                     pendingWithdrawal[player_addresses[i]]+=bet+(shared_pot/(ignorers+1));
                 }
             }
+            reset_game()
+        }
+        //delete players_vote[address]
+    }
+
+    function reset_game() {
+        current_player = 0;
+        for (uint i=0; i < player_total; i++){
+            delete players_vote[player_addresses[i]];
         }
     }
 }
